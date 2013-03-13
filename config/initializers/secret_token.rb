d3d421ba16eb18f22f8663ebc45aa9f2ce0ec29f
Matthew Follett <matthew.follett@zedit.com>
2013-03-13 09:38:58 -0230

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foruby::Application.config.secret_token = 'd45e60e3a4a1e3b442888c5f5a2d6b6bf845989a0a9eb89cec7f5ff03712a5d17e2c27d61c78207cd423da1e72e985ac8047ac67be447058beff42593c90e706'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Foruby::Application.config.session_store :cookie_store, key: '_foruby_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Foruby::Application.config.session_store :active_record_store
